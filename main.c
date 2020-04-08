a769018c63b09848c58e546acc3a3d17752cc0a7
Sivaprakashpasupathibalan <61182754+Sivaprakashpasupathibalan@users.noreply.github.com>
2020-04-08 21:50:33 +0530

    Proteus VSM Interface and PORTB setup
    LCD interface in proteus design is done and enabling the portB pin dir and en according to interface.

--- main.c
+++ main.c
@@ -9,15 +9,18 @@ Author: Shivaprakash Pasupathibalan
 #define PORTB_DIR        (*((volatile unsigned long *)0x40005400U))      //PORTB Direction Control Register ADDRESS
 #define PORTB_DATA_EN    (*((volatile unsigned int *)0x4000551CU))       //PORTB Data Enable Register
 #define PORTB_DATA       (*((volatile unsigned long *)0x400053fcU))      //PORTB Data Control Register ADDRESS
+#define RS  (1 << 0)
+#define EN  (1 << 1)
+#define DATA_DIR (0x0F << 2)                                                   //Data Lines For LCD 16x2
 int main()
 {
   RCGC2 |= (1 << 1);                             //Setting Bitfield 1 for PORTB Enable
-  PORTB_DIR = 0xFF;                              //Setting Direction Bit for PORTB
-  PORTB_DATA_EN = 0xFF;                          //Setting Data Enable for PORTB
+  PORTB_DIR |= (DATA_DIR | RS | EN);                              //Setting Direction Bit for PORTB
+  PORTB_DATA_EN |= (DATA_DIR | RS | EN);                          //Setting Data Enable for PORTB
   while(1)                                       //Turn ON & OFF PORTB bits at particular interval
 {
   for(int i = 0; i < 400; i++);  
-  PORTB_DATA = 0xFF;
+  PORTB_DATA |= (DATA_DIR | RS | EN);
   for(int i = 0; i < 400; i++);
   PORTB_DATA = 0x00;
   for(int i = 0; i < 400; i++);
