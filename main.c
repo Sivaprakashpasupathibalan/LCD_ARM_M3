c6304efebeca74ff36a783320f73329a4bf2b69d
Sivaprakashpasupathibalan <61182754+Sivaprakashpasupathibalan@users.noreply.github.com>
2020-04-08 20:31:17 +0530

    PORTB VSM PROTEUS ERROR SOLVED
    PORTB Data Enable Memory address is set and output in proteus file is working

--- main.c
+++ main.c
@@ -5,19 +5,21 @@ Firmware Version : 1
 Author: Shivaprakash Pasupathibalan
 @ 08 April 2020
 */ 
-#define RCGC2       (*((volatile unsigned long *)0x400FE108))      //ClockGating Control Register For Run Mode GPIO ADDRESS
-#define PORTB_DIR   (*((volatile unsigned long *)0x40005400))      //PORTB Direction Control Register ADDRESS
-#define PORTB_DATA  (*((volatile unsigned long *)0x400053fc))      //PORTB Data Control Register ADDRESS
+#define RCGC2            (*((volatile unsigned long *)0x400FE108U))      //ClockGating Control Register For Run Mode GPIO ADDRESS
+#define PORTB_DIR        (*((volatile unsigned long *)0x40005400U))      //PORTB Direction Control Register ADDRESS
+#define PORTB_DATA_EN    (*((volatile unsigned int *)0x4000551CU))       //PORTB Data Enable Register
+#define PORTB_DATA       (*((volatile unsigned long *)0x400053fcU))      //PORTB Data Control Register ADDRESS
 int main()
 {
-  RCGC2 |= (1 << 1);                              //Setting Bitfield 1 for PORTB Enable
-  PORTB_DIR = 0xFFFFFFFF;                         //Setting Direction Bit for PORTB
-  while(1)                                        //Turn ON & OFF PORTB bits at particular interval
+  RCGC2 |= (1 << 1);                             //Setting Bitfield 1 for PORTB Enable
+  PORTB_DIR = 0xFF;                              //Setting Direction Bit for PORTB
+  PORTB_DATA_EN = 0xFF;                          //Setting Data Enable for PORTB
+  while(1)                                       //Turn ON & OFF PORTB bits at particular interval
 {
   for(int i = 0; i < 400; i++);  
-  PORTB_DATA = 0xFFFFFFFF;
+  PORTB_DATA = 0xFF;
   for(int i = 0; i < 400; i++);
-  PORTB_DATA = 0x00000000;
+  PORTB_DATA = 0x00;
   for(int i = 0; i < 400; i++);
 }
   return 0;
